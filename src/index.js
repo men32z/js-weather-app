6a4fe342ac75417bf1a2da2cd0a98dfc6ab8735d
Luis Preza <preza.luis@gmail.com>
2020-01-30 18:30:54 -0600

    function celsius just console log now

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-async function getCelsius(location){
+const weather = (() => {
+  let appid = 'a21ef4017ff27a42723559dd93f1f28e';
 
-}
+  const getCelsius = async (location) => {
+    let endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${appid}`;
+    let response = await fetch(endpoint, {mode: 'cors'});
+    return response.json();
+  }
 
-async function getFahrenheit(location){
-  
-}
+  const getFahrenheit = async (location) =>{
+
+  }
+
+  return {getCelsius, getFahrenheit};
+})();
+
+weather.getCelsius("Guadalajara").then(res => console.log(res));
