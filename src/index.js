67bd58f9d0e15c98f658d0eb9d4d8229dfd8df6e
Luis Preza <preza.luis@gmail.com>
2020-01-31 15:27:57 -0600

    scss now compiling with webpack, bootstrap and weather icons installed

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import Weather from './js/weather';
 
 //let weather = new WeatherService();
 //weather.getCelsius("Guadalajara").then(res => console.log(JSON.stringify(res)));
-let celsius = new Weather({"coord":{"lon":-103.33,"lat":20.67},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"stations","main":{"temp":18.04,"feels_like":16.48,"temp_min":17,"temp_max":20,"pressure":1022,"humidity":55},"visibility":9656,"wind":{"speed":1.87,"deg":209},"clouds":{"all":75},"dt":1580495146,"sys":{"type":1,"id":7128,"country":"MX","sunrise":1580477408,"sunset":1580517789},"timezone":-21600,"id":4005539,"name":"Guadalajara","cod":200});
-console.log(celsius);
+//let celsius = new Weather({"coord":{"lon":-103.33,"lat":20.67},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"stations","main":{"temp":18.04,"feels_like":16.48,"temp_min":17,"temp_max":20,"pressure":1022,"humidity":55},"visibility":9656,"wind":{"speed":1.87,"deg":209},"clouds":{"all":75},"dt":1580495146,"sys":{"type":1,"id":7128,"country":"MX","sunrise":1580477408,"sunset":1580517789},"timezone":-21600,"id":4005539,"name":"Guadalajara","cod":200});
+//console.log(celsius);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['./src/index.js', './src/css/style.scss'],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loader: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }),
+  ],
 };
